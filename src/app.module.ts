b6fe8d96f2cf1705cf1df56034d79e1026636efc
camb212 <camila.benavidez@ute.edu.ec>
2025-07-17 21:53:15 -0500

    Avanzado en clase

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -11,7 +13,25 @@ import { OrdersModule } from './orders/orders.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/ghibli-db'),
+    ConfigModule.forRoot({ isGlobal: true }),
+
+    // PostgreSQL con TypeORM
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT || '5432', 10),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+      ssl: false,
+    }),
+
+    // MongoDB con Mongoose
+    MongooseModule.forRoot(process.env.MONGO_URI),
+
+    // Módulos de tu app
     AuthModule,
     UsersModule,
     PlushiesModule,
@@ -23,3 +43,4 @@ import { OrdersModule } from './orders/orders.module';
   providers: [AppService],
 })
 export class AppModule {}
+
