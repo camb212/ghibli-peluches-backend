9b06bda548c14c782e148c3a427a9c588d7c75b0
camb212 <camila.benavidez@ute.edu.ec>
2025-07-24 11:20:50 -0500

    Correccion

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Habilita CORS para permitir peticiones desde cualquier origen
+  app.enableCors({
+    origin: '*', // O puedes especificar un dominio exacto como: 'https://mi-frontend.vercel.app'
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
   app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(3010);
 }
 bootstrap();
