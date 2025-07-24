4beb7e62195d57382a5f5ab7719f1b2557dc5545
camb212 <camila.benavidez@ute.edu.ec>
2025-07-24 01:28:15 -0500

    Correccion

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MongooseModule } from '@nestjs/mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { PlushiesModule } from './plushies/plushies.module';
@@ -20,25 +22,25 @@ import { OrdersModule } from './orders/orders.module';
       type: 'postgres',
       host: process.env.DB_HOST,
       port: parseInt(process.env.DB_PORT || '5432', 10),
-      username: process.env.DB_USER,
-      password: process.env.DB_PASS,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
-      entities: [__dirname + '/**/*.typeorm.entity{.ts,.js}'], // Only TypeORM entities
+      entities: [__dirname + '/**/*.typeorm.entity{.ts,.js}'], 
       synchronize: true,
       ssl: {
-        rejectUnauthorized: false
+        rejectUnauthorized: false,
       },
       extra: {
         ssl: {
-          rejectUnauthorized: false
-        }
-      }
+          rejectUnauthorized: false,
+        },
+      },
     }),
 
     // MongoDB con Mongoose - Using Atlas Cloud Database
     MongooseModule.forRoot(process.env.MONGO_URI || 'mongodb://localhost:27017/ghibli'),
 
-    // Módulos de tu app
+    // Módulos de la app
     AuthModule,
     UsersModule,
     PlushiesModule,
